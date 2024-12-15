d24fa0db99bd7b63235af952173d33dbf7d9b554
Avinendra <avinendra08@gmail.com>
2024-12-15 12:24:44 +0530

    backend setup,user model

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userType: {
+    type: String,
+    enum: ["student", "professor"],
+    required: true,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
